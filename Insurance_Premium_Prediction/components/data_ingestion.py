0623306fc8001963967a47143b5868447cd101f8
Yash Dahekar <yashdahekar92@gmai.com>
2024-01-30 01:18:14 +0530

    completed model training

---
+++ Insurance_Premium_Prediction/components/data_ingestion.py
@@ -0,0 +1,8 @@
+class DataIngestion:
+    def __init__(self):
+        pass
+    
+    def initiate_data_inestion(self):
+        pass
+    
+    
---
+++ Insurance_Premium_Prediction/components/model_trainer.py
@@ -0,0 +1,8 @@
+class ModelTrainer:
+    def __init__(self):
+        pass
+    
+    def get_data_transformation(self):
+        pass
+    
+    
